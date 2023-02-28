871ed709993a68a640c48afa0b895ab747e777fc
xansec <76011430+xansec@users.noreply.github.com>
2023-02-28 12:17:31 -0500

    Merge pull request #1 from ennamarie19/mayhem
    Mayhem Heroes Integration

---
+++ mayhem/fuzz_twb.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import random
+
+import atheris
+import sys
+
+
+import fuzz_helpers
+import random
+
+with atheris.instrument_imports(include=['tableaudocumentapi']):
+    from tableaudocumentapi import Workbook
+
+from lxml.etree import XMLSyntaxError
+from tableaudocumentapi.xfile import TableauVersionNotSupportedException, TableauInvalidFileException
+
+def TestOneInput(data):
+    fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
+    try:
+        with fdp.ConsumeTemporaryFile('.twb', all_data=True, as_bytes=True) as filename:
+            Workbook(filename)
+    except (XMLSyntaxError, TableauVersionNotSupportedException, ValueError, AttributeError, TableauInvalidFileException):
+        return -1
+    except TypeError:
+        if random.random() > .9:
+            raise
+        return -1
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
